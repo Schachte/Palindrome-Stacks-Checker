4e93a09292178a962ed48220c19ee772f0dd4c53
Schachte <rschacht@asu.edu>
2014-04-23 13:57:52 -0700

    Stacks Palindrome Checker
    Takes character by character user input into a stack and determines if
    word is a palindrome.

---
+++ PalindromeStacks.java
@@ -0,0 +1,62 @@
+import java.util.*;
+/*
+ * Ryan Schachte
+ * Stack implementation
+ * Build a program to continuously accept stacks until user enters "end"
+ * Stores stack forward order and stack reverse order into two seperate string and determines if word is a palindrome
+ */
+
+public class PalindromeStacks {
+	
+	public static void main(String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		Stack<String> stackInformation = new Stack<String>();
+		String forward  = "";
+		String reversed = "";
+		
+		System.out.println("Enter a word with a new character on each line: \n\nEnter \"end\" to finalize the word!");
+		
+		String stackAdd = input.nextLine();
+		
+		while (!stackAdd.equalsIgnoreCase("end"))
+		{
+			stackInformation.push(stackAdd);
+			
+			stackAdd = input.nextLine();
+
+		}
+		
+		Iterator<String> iter = stackInformation.iterator();
+		
+		System.out.print("Your word is: ");
+		while(iter.hasNext())
+		{
+			forward += iter.next();
+		}
+		System.out.print(forward);
+		
+		System.out.println("");
+		
+		System.out.print("Your reversed word is: ");
+		while(!stackInformation.isEmpty())
+		{
+			reversed+=stackInformation.peek();
+			stackInformation.pop();
+		}
+		
+		System.out.print(reversed);
+		
+		System.out.println("");
+
+		if (forward.equalsIgnoreCase(reversed))
+		{
+			System.out.println("This is a palindrome!");
+		}
+		else
+		{
+			System.out.println("This is not a palindrome..");
+		}
+	}
+
+}
